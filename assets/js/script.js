e96c11ddcae39f4f5e038540a44c4a70ad947a49
Lavken <lavkenpilar@gmail.com>
2024-02-06 20:25:32 -0300

    create-index-js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const porDos = (num)=>{
+return num *2
+}
+
+const resultado = porDos (10)
+console.log (resultado)
+
+const porTres = (num)=>{
+    return num*3
+}
+
+const resultado2 = porTres (10)
+console.log (resultado2)
+
+const multiplicar = (num1, num2)=>{
+return num1 * num2
+}
+const resultado3= multiplicar (5,10)
+console.log (resultado3)
